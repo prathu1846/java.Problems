f4042447b7f1cc50d91967e32b96d8844544af8b
prathu1846 <159513648+prathu1846@users.noreply.github.com>
2024-03-16 22:59:09 +0530

    Add files via upload

---
+++ java/practice3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class practice3 {
+
+    public static void main(String args[]) {
+
+        int n,fact=1;
+        System.out.println("Enter the number");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+
+        for(int i=1;i<=n;i++) {
+            fact = fact * i;
+        }
+        System.out.println(" " +fact);
+
+    }
+}
